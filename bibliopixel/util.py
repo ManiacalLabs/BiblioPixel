9542735e41135e7e830e252f318f84197fcbf31b
Tom Ritchford <tom@swirly.com>
2016-02-21 12:31:08 -0500

    Simplify util.d (#35).

--- bibliopixel/util.py
+++ bibliopixel/util.py
@@ -1,20 +1,9 @@
-class d(dict):
+class AttributeDict(dict):
+    """A dict that exposes its values as attributes."""
+    def __getattr__(self, key):
+        return self[k]
 
-    def __init__(self, *a, **k):
-        super(d, self).__init__(*a, **k)
-        self.__dict__ = self
-        for k in self.__dict__:
-            if isinstance(self.__dict__[k], dict):
-                self.__dict__[k] = d(self.__dict__[k])
-            elif isinstance(self.__dict__[k], list):
-                for i in range(len(self.__dict__[k])):
-                    if isinstance(self.__dict__[k][i], dict):
-                        self.__dict__[k][i] = d(self.__dict__[k][i])
-
-    def upgrade(self, a):
-        for k, v in a.items():
-            if k not in self:
-                self[k] = v
+d = AttributeDict
 
 
 def tuple_add(a, b):
