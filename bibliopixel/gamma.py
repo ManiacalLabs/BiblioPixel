5f314b20df2b6d051aae4d2b2656344ce97bb163
Tom Ritchford <tom@swirly.com>
2016-08-02 11:24:39 -0400

    Remove old gamma tables.

--- bibliopixel/gamma.py
+++ bibliopixel/gamma.py
@@ -7,15 +7,3 @@ WS2801 = SM16716 = _table()
 
 # From http://rgb-123.com/ws2812-color-output/
 WS2812B = NEOPIXEL = WS2812 = _table(gamma=1 / 0.45)
-
-# Previous constants.
-# From https://github.com/scottjgibson/PixelPi/blob/master/pixelpi.py
-_LPD8806 = [int(pow(float(i) / 255.0, 2.5) * 255.0 + 0.5) for i in range(256)]
-_WS2801 = [int(pow(float(i) / 255.0, 2.5) * 255.0) for i in range(256)]
-
-# From http://rgb-123.com/ws2812-color-output/
-_WS2812B = [int(pow(float(i) / 255.0, 1.0 / 0.45) * 255.0) for i in range(256)]
-
-assert tuple(_LPD8806) == LPD8806
-assert tuple(_WS2801) == WS2801
-assert tuple(_WS2812B) == WS2812
