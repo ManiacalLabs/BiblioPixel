b4967877cdbf8b82889273f9d00c5d31b27194b3
Tom Ritchford <tom@swirly.com>
2016-12-16 17:04:06 +0100

    Split off led/strip.py

--- bibliopixel/led/__init__.py
+++ bibliopixel/led/__init__.py
@@ -1,2 +1,119 @@
-from . led import LEDStrip, MatrixRotation, mapGen, MultiMapBuilder, LEDMatrix
-from . led import LEDPOV, LEDCircle, MANIFEST
+from . led import MatrixRotation, mapGen, MultiMapBuilder, LEDMatrix
+from . led import LEDPOV, LEDCircle
+from . strip import LEDStrip
+
+
+MANIFEST = [
+    {
+        "id": "strip",
+        "class": LEDStrip,
+        "type": "controller",
+        "control_type": "strip",
+        "display": "LEDStrip",
+        "params": [{
+                "id": "threadedUpdate",
+                "label": "Threaded Update",
+                "type": "bool",
+                "default": False,
+                "help": "Enable to run display updates on a separate thread, which can improve speed."
+        }, {
+            "id": "masterBrightness",
+            "label": "Master Brightness",
+            "type": "int",
+            "min": 1,
+            "max": 255,
+            "default": 255,
+            "help": "Master brightness for display, 0-255"
+        }, {
+            "id": "pixelWidth",
+            "label": "Pixel Width",
+            "group": "Advanced",
+            "type": "int",
+            "min": 1,
+            "default": 1,
+            "help": "Pixel scaling amount. Setting to >1 will make each logical pixel N LEDs in width."
+        }, ]
+    },
+    {
+        "id": "matrix",
+        "class": LEDMatrix,
+        "type": "controller",
+        "control_type": "matrix",
+        "display": "LEDMatrix",
+        "params": [{
+                "id": "width",
+                "label": "Width",
+                "type": "int",
+                "default": 0,
+                "min": 0,
+                "help": "Width of display. Leave this and Height as 0 to get from driver."
+        }, {
+            "id": "height",
+            "label": "Height",
+            "type": "int",
+            "default": 0,
+            "min": 0,
+            "help": "Height of display. Leave this and Width as 0 to get from driver."
+        }, {
+            "id": "rotation",
+            "label": "Rotation",
+            "type": "combo",
+            "options": {
+                    0: "0&deg;",
+                    3: "90&deg;",
+                    2: "180&deg;",
+                    1: "270&deg;",
+            },
+            "default": 0,
+            "help": "Amount to rotate matrix by."
+        }, {
+            "id": "vert_flip",
+            "label": "Vertical Flip",
+            "type": "bool",
+            "default": False,
+            "help": "Flip the display over the x-axis.",
+        }, {
+            "id": "serpentine",
+            "label": "Serpentine",
+            "type": "bool",
+            "default": True,
+            "help": "Use a serpentine layout for the pixel map when auto-generated.",
+        }, {
+            "id": "threadedUpdate",
+            "label": "Threaded Update",
+            "type": "bool",
+            "default": False,
+            "help": "Enable to run display updates on a separate thread, which can improve speed."
+        }, {
+            "id": "masterBrightness",
+            "label": "Master Brightness",
+            "type": "int",
+            "min": 1,
+            "max": 255,
+            "default": 255,
+            "help": "Master brightness for display, 0-255"
+        }, {
+            "id": "pixelSize",
+            "label": "Pixel Size",
+            "group": "Advanced",
+            "type": "multi",
+            "controls": [
+                    {
+                        "label": "Width",
+                        "type": "int",
+                        "min": 1,
+                        "default": 1,
+                        "help": "Logical Pixel Width"
+                    }, {
+                        "label": "Height",
+                        "type": "int",
+                        "min": 1,
+                        "default": 1,
+                        "help": "Logical Pixel Height"
+                    },
+            ],
+            "default": [1, 1],
+            "help":"Pixel scaling amount. Each logical pixel will be Width*Height LEDs in size."
+        }, ]
+    }
+]
---
+++ bibliopixel/led/strip.py
@@ -0,0 +1,50 @@
+from .. import colors
+from . base import LEDBase
+
+
+class LEDStrip(LEDBase):
+
+    def __init__(self, drivers, threadedUpdate=False, masterBrightness=255, pixelWidth=1):
+        super().__init__(drivers, threadedUpdate, masterBrightness)
+
+        self.pixelWidth = pixelWidth
+        if self.pixelWidth < 1 or self.pixelWidth > self.numLEDs:
+            raise ValueError(
+                "pixelWidth must be greater than 0 and less than or equal to the total LED count!")
+        if self.numLEDs % self.pixelWidth != 0:
+            raise ValueError(
+                "Total LED count must be evenly divisible by pixelWidth!")
+        if self.pixelWidth == 1:
+            self.set = self._set
+        else:
+            self.set = self._setScaled
+            self.numLEDs = self.numLEDs / self.pixelWidth
+
+    # Set single pixel to Color value
+    def _set(self, pixel, color):
+        """Set pixel to RGB color tuple"""
+        self._set_base(pixel, color)
+
+    def _setScaled(self, pixel, color):
+        start = pixel * self.pixelWidth
+        for p in range(start, start + self.pixelWidth):
+            self._set_base(p, color)
+
+    def get(self, pixel):
+        """Get RGB color tuple of color at index pixel"""
+        return self._get_base(pixel)
+
+    # Set single pixel to RGB value
+    def setRGB(self, pixel, r, g, b):
+        """Set single pixel using individual RGB values instead of tuple"""
+        self.set(pixel, (r, g, b))
+
+    def setHSV(self, pixel, hsv):
+        """Set single pixel to HSV tuple"""
+        color = colors.hsv2rgb(hsv)
+        self.set(pixel, color)
+
+    # turns off the desired pixel
+    def setOff(self, pixel):
+        """Set single pixel off"""
+        self.set(pixel, (0, 0, 0))
