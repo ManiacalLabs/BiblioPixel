65581b87df87dfbd57f4a80daf4bdb7fb30f0878
adammhaile <adammhaile@gmail.com>
2016-12-18 14:44:23 -0500

    Adding BaseReceiver class from BP2.

--- bibliopixel/animation/__init__.py
+++ bibliopixel/animation/__init__.py
@@ -3,4 +3,5 @@ from . game import BaseGameAnim
 from . off import OffAnim
 from . queue import AnimationQueue
 from . strip import BaseStripAnim
+from . receiver import BaseReceiver
 from . tests import StripChannelTest, MatrixChannelTest, MatrixCalibrationTest
