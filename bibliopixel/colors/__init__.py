35d15ab15ba819dd122af1fff17785f9262cdccb
Tom Ritchford <tom@swirly.com>
2016-12-18 13:17:56 +0100

    Extract colors/arithmetic.py.

--- bibliopixel/colors/__init__.py
+++ bibliopixel/colors/__init__.py
@@ -46,19 +46,8 @@ More details here: https://github.com/FastLED/FastLED/wiki/FastLED-HSV-Colors
 """
 
 import colorsys, types, util
+from . arithmetic import color_blend, color_scale
 from . wheel import wheel_color, wheel_helper
-
-
-def color_scale(color, level):
-    """Scale RGB tuple by level, 0 - 255"""
-    return tuple([(i * level) >> 8 for i in list(color)])
-
-
-def color_blend(a, b):
-    """Performs a Screen blend on RGB color tuples, a and b"""
-    return (255 - (((255 - a[0]) * (255 - b[0])) >> 8),
-            255 - (((255 - a[1]) * (255 - b[1])) >> 8),
-            255 - (((255 - a[2]) * (255 - b[2])) >> 8))
 
 
 def hsv2rgb_raw(hsv):
---
+++ bibliopixel/colors/arithmetic.py
@@ -0,0 +1,10 @@
+def color_blend(a, b):
+    """Performs a Screen blend on RGB color tuples, a and b"""
+    return (255 - (((255 - a[0]) * (255 - b[0])) >> 8),
+            255 - (((255 - a[1]) * (255 - b[1])) >> 8),
+            255 - (((255 - a[2]) * (255 - b[2])) >> 8))
+
+
+def color_scale(color, level):
+    """Scale RGB tuple by level, 0 - 255"""
+    return tuple([(i * level) >> 8 for i in list(color)])
