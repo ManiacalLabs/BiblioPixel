52f849ebf6ce3fc708e2a1e59c2601424dffa6a9
adammhaile <adammhaile@gmail.com>
2016-12-18 18:36:26 -0500

    Adding support for Travis and adding test and benchmark cmdclass to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,36 @@
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 import bibliopixel
+
+
+# from here: http://pytest.org/2.2.4/goodpractises.html
+# this and RunBenchmark are near identical
+# TODO: Find a way to combine these
+class RunTests(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['tests']
+        self.test_suite = True
+
+    def run_tests(self):
+        #  import here, cause outside the eggs aren't loaded
+        import pytest
+        pytest.main(self.test_args)
+
+
+class RunBenchmark(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = ['benchmarks']
+        self.test_suite = True
+
+    def run_tests(self):
+        #  import here, cause outside the eggs aren't loaded
+        import pytest
+        pytest.main(self.test_args)
+
 
 setup(
     name='BiblioPixel',
@@ -13,8 +44,12 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.5',
     ],
+    tests_require=['pytest'],
+    cmdclass={
+        'test': RunTests,
+        'benchmark': RunBenchmark
+    }
 )
