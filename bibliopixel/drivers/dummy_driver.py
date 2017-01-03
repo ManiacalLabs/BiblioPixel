e3e6420c7df8a5a189e98ac40ed1b8dc0df7df40
Tom Ritchford <tom@swirly.com>
2017-01-03 14:10:03 +0100

    DummyDriver now accepts any keyword arguments.

--- bibliopixel/drivers/dummy_driver.py
+++ bibliopixel/drivers/dummy_driver.py
@@ -5,7 +5,7 @@ import time
 class DriverDummy(DriverBase):
     """For Testing: Provides no ouput, just a valid interface"""
 
-    def __init__(self, num, delay=0):
+    def __init__(self, num, delay=0, **_):
         """delay: time to wait in milliseconds to simulate actual hardware interface time"""
         super(DriverDummy, self).__init__(num)
         self._delay = delay
