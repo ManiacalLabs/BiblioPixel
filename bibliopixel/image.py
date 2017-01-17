4f7a07c4170aaebae8e0bfefffc866105678eb7f
Tom Ritchford <tom@swirly.com>
2017-01-17 13:19:28 +0100

    Use Python lists in image.py

--- bibliopixel/image.py
+++ bibliopixel/image.py
@@ -1,5 +1,5 @@
 import glob, numbers, os, sys
-from . import colors, log, color_list
+from . import colors, log
 from . led import LEDMatrix
 
 try:
@@ -86,12 +86,12 @@ def convert_mode(image, mode='RGB'):
     return image if (image.mode == mode) else image.convert(mode=mode)
 
 
-def image_to_colorlist(image, container=color_list.ColorList):
+def image_to_colorlist(image, container=list):
     """Given a PIL.Image, returns a ColorList of its pixels."""
     return container(convert_mode(image).getdata())
 
 
-def animated_gif_to_colorlists(image, container):
+def animated_gif_to_colorlists(image, container=list):
     """Given an animated GIF, return a list with a colorlist for each frame."""
     it = ImageSequence.Iterator(image)
     return [image_to_colorlist(i, container) for i in it]
