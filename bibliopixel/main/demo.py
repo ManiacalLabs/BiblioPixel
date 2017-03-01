c0e8cb02af7bff53d58915db1032be10d30f3dce
Tom Ritchford <tom@swirly.com>
2017-03-01 20:13:23 +0100

    Now demo does one thing.

--- bibliopixel/main/demo.py
+++ bibliopixel/main/demo.py
@@ -1,3 +1,33 @@
+import webbrowser
+
+from bibliopixel.drivers.SimPixel import DriverSimPixel
+from bibliopixel import LEDMatrix
+
+SIMPIXEL_URL = 'http://beta.simpixel.io'
+IMPORT_ERROR_TEXT = """
+Please install the BiblioPixelAnimations library from here:
+
+https://github.com/ManiacalLabs/BiblioPixelAnimations
+
+"""
+
+
+def run(args):
+    try:
+        from BiblioPixelAnimations.matrix.bloom import Bloom
+    except ImportError:
+        raise ValueError(IMPORT_ERROR_TEXT)
+
+    driver = DriverSimPixel(args.x * args.y)
+    led = LEDMatrix(driver, width=args.x, height=args.y)
+    anim = Bloom(led)
+
+    webbrowser.open(SIMPIXEL_URL, new=0, autoraise=True)
+    anim.run()
+
+
 def set_parser(parser):
-    parser.set_defaults(run=print)
-    parser.add_argument('-name', type=str, default='bloom')
+    parser.set_defaults(run=run)
+    parser.add_argument('-name', default='bloom')
+    parser.add_argument('-x', default=32, type=int)
+    parser.add_argument('-y', default=32, type=int)
