4a8ba27287158eb11f6c891400b3e00e9666dc0a
Tom Ritchford <tom@swirly.com>
2017-03-11 20:35:26 +0100

    Tiny cleanup to spi_driver_base.py.

--- bibliopixel/drivers/spi_driver_base.py
+++ bibliopixel/drivers/spi_driver_base.py
@@ -1,34 +1,33 @@
+import os, sys, time
 from . driver_base import DriverBase, ChannelOrder
-import time
-
-import os
 from .. import log
 
 
 class DriverSPIBase(DriverBase):
-    """Base driver for controling SPI devices on systems like the Raspberry Pi and BeagleBone"""
-
+    """
+    Base driver for controling SPI devices on systems like the Raspberry Pi and
+    BeagleBone.
+    """
     def __init__(self, num, c_order=ChannelOrder.GRB, use_py_spi=True,
-                 dev="/dev/spidev0.0", SPISpeed=2, open=open, gamma=None):
+                 dev='/dev/spidev0.0', SPISpeed=2, open=open, gamma=None):
         super().__init__(num, c_order=c_order, gamma=gamma)
 
         self.dev = dev
         self.use_py_spi = use_py_spi
         self._spiSpeed = SPISpeed
+        self._open = open
 
         # File based SPI requires a bytearray so we have to overwrite _buf
         if self.use_py_spi:
             a, b = -1, -1
-            d = self.dev.replace("/dev/spidev", "")
+            d = self.dev.replace('/dev/spidev', '')
             s = d.split('.')
             if len(s) == 2:
                 a = int(s[0])
                 b = int(s[1])
 
             if a < 0 or b < 0:
-                error = "When using py-spidev, the given device must be in the format /dev/spidev*.*"
-                log.error(error)
-                raise ValueError(error)
+                error(BAD_FORMAT_ERROR)
 
             self._bootstrapSPIDev()
             import spidev
@@ -39,34 +38,25 @@ class DriverSPIBase(DriverBase):
                      (float(self.spi.max_speed_hz) / 1000000.0))
         else:
             self._buf = bytearray(self._buf)
-            self.spi = open(self.dev, "wb")
+            self.spi = self._open(self.dev, 'wb')
 
     def _bootstrapSPIDev(self):
-        import os.path
-        import sys
         if self.use_py_spi:
             try:
                 import spidev
             except:
-                error = "Unable to import spidev. Please install. pip install spidev"
-                log.error(error)
-                raise ImportError(error)
+                error(CANT_IMPORT_SPIDEV_ERROR)
 
         if not os.path.exists(self.dev):
-            error = "Cannot find SPI device. Please see https://github.com/maniacallabs/bibliopixel/wiki/SPI-Setup for details."
-            log.error(error)
-            raise IOError(error)
+            error(CANT_FIND_ERROR)
 
         # permissions check
         try:
-            open(self.dev)
+            self.open(self.dev)
         except IOError as e:
             if e.errno == 13:
-                error = "Cannot find SPI device. Please see https://github.com/maniacallabs/bibliopixel/wiki/SPI-Setup for details."
-                log.error(error)
-                raise IOError(error)
-            else:
-                raise e
+                error(CANT_FIND_ERROR)
+            raise e
 
     def _send_packet(self):
         if self.use_py_spi:
@@ -78,3 +68,26 @@ class DriverSPIBase(DriverBase):
     def _compute_packet(self):
         self._render()
         self._packet = self._buf
+
+
+CANT_FIND_ERROR = """Cannot find SPI device.
+Please see
+
+    https://github.com/maniacallabs/bibliopixel/wiki/SPI-Setup
+
+for details.
+"""
+
+BAD_FORMAT_ERROR = (
+    'When using py-spidev, `dev` must be in the format /dev/spidev*.*')
+
+CANT_IMPORT_SPIDEV_ERROR = """
+Unable to import spidev. Please install:
+
+    pip install spidev
+"""
+
+
+def error(text):
+    log.error(text)
+    raise IOError(text)
