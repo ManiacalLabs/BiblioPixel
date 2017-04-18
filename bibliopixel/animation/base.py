4aa18309a87500d09812d3b61edb5528aa8d8012
Tom Ritchford <tom@swirly.com>
2017-04-18 17:56:12 +0200

    Refactor BaseAnimation.run.

--- bibliopixel/animation/base.py
+++ bibliopixel/animation/base.py
@@ -36,58 +36,61 @@ class BaseAnimation(object):
 
         return not (self.runner.until_complete and self.animComplete)
 
-    def _run(self):
-        if self.free_run:
-            self.sleep_time = None
-        elif self._internalDelay:
-            self.sleep_time = self._internalDelay
-        else:
-            self.sleep_time = self.runner.sleep_time
+    def _run_once(self):
+        self._timeRef = self._msTime()
 
+        self.start = self._msTime()
+        self.step(self.runner.amt)
+        self.mid = self._msTime()
+
+        self._led.frame_render_time = int(self.mid - self.start)
+        self._led.animation_sleep_time = self.sleep_time or 0
+
+        self._led.push_to_driver()
+        self.now = self._msTime()
+
+        if self.animComplete and self.runner.max_cycles > 0:
+            if self.cycle_count < self.runner.max_cycles - 1:
+                self.cycle_count += 1
+                self.animComplete = False
+
+        self.threading.report_framerate(self.start, self.mid, self.now)
+
+        if self.sleep_time:
+            diff = (self._msTime() - self._timeRef)
+            t = max(0, self.sleep_time - diff)
+            if t == 0:
+                log.warning('Frame-time of %dms set, but took %dms!',
+                            self.sleep_time, diff)
+            self.threading.wait(t)
+        self.cur_step += 1
+
+    def _run(self):
         self.preRun(self.runner.amt)
+
+        while self._is_running():
+            self._run_once()
+
+    def run(self, **kwds):
+        def run():
+            try:
+                self._run()
+            finally:
+                self.cleanup()
+
+        self.runner = Runner(**kwds)
 
         self._step = 0
         self.cur_step = 0
         self.cycle_count = 0
         self.animComplete = False
 
-        while self._is_running():
-            self._timeRef = self._msTime()
-
-            self.start = self._msTime()
-            self.step(self.runner.amt)
-            self.mid = self._msTime()
-
-            self._led.frame_render_time = int(self.mid - self.start)
-            self._led.animation_sleep_time = self.sleep_time or 0
-
-            self._led.push_to_driver()
-            self.now = self._msTime()
-
-            if self.animComplete and self.runner.max_cycles > 0:
-                if self.cycle_count < self.runner.max_cycles - 1:
-                    self.cycle_count += 1
-                    self.animComplete = False
-
-            self.threading.report_framerate(self.start, self.mid, self.now)
-
-            if self.sleep_time:
-                diff = (self._msTime() - self._timeRef)
-                t = max(0, self.sleep_time - diff)
-                if t == 0:
-                    log.warning('Frame-time of %dms set, but took %dms!',
-                                self.sleep_time, diff)
-                self.threading.wait(t)
-            self.cur_step += 1
-
-    def run(self, **kwds):
-        self.runner = Runner(**kwds)
-
-        def run():
-            try:
-                self._run()
-            finally:
-                self.cleanup()
+        if self.free_run:
+            self.sleep_time = None
+        elif self._internalDelay:
+            self.sleep_time = self._internalDelay
+        else:
+            self.sleep_time = self.runner.sleep_time
 
         self.threading = AnimationThreading(self.runner)
         self.threading.run_animation(run)
