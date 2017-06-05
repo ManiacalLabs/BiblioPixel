ac841b451feadc6b061a5561409c0922443d8a8b
TuxCoder <git@o-g.at>
2017-06-05 13:13:45 +0200

    format fix for WS2812SPI

--- bibliopixel/drivers/WS2812SPI.py
+++ bibliopixel/drivers/WS2812SPI.py
@@ -3,9 +3,14 @@ from .. import gamma as _gamma
 
 
 class DriverWS2812SPI(DriverSPIBase):
-    """SPI driver for WS2812(b) based LED strips on devices like the Raspberry Pi, OrangePi, BeagleBone,.."""
+    """
+    SPI driver for WS2812(b) based LED strips on devices like
+    Raspberry Pi, OrangePi, BeagleBone,..
+    """
+
     def __init__(self, num, **kwargs):
-        super(DriverWS2812SPI, self).__init__(num, c_order=ChannelOrder.GRB, SPISpeed=3, gamma=_gamma.WS2812, **kwargs)
+        super().__init__(num, c_order=ChannelOrder.GRB, SPISpeed=3,
+                         gamma=_gamma.WS2812, **kwargs)
 
     # WS2812 requires gamma correction so we run it through gamma as the
     # channels are ordered
@@ -14,10 +19,11 @@ class DriverWS2812SPI(DriverSPIBase):
     # 0b0  -> 0b100
     # 0b1  -> 0b110
     def _compute_packet(self):
-        super(DriverWS2812SPI, self)._compute_packet()
+        super()._compute_packet()
 
         # buffer for result
-        buf2 = []
+        # [0] fixes, first led show green when should be off
+        buf2 = [0]
         for byte in self._buf:
             # save each byte in an int var
             tmp = 0
@@ -28,8 +34,8 @@ class DriverWS2812SPI(DriverSPIBase):
             buf2.append(tmp >> 8 & 0xff)
             buf2.append(tmp & 0xff)
 
-        # [0] fixes, first led show green when should be off
-        self._packet = [0] + buf2
+        self._packet = buf2
+
 
 MANIFEST = [
     {
@@ -39,12 +45,12 @@ MANIFEST = [
         "display": "WS2812 (SPI)",
         "desc": "Interface with WS2812 / WS2812B strips over a native SPI port (Pi, BeagleBone, etc.)",
         "params": [{
-                "id": "num",
-                "label": "# Pixels",
-                "type": "int",
-                "default": 1,
-                "min": 1,
-                "help": "Total pixels in display."
+            "id": "num",
+            "label": "# Pixels",
+            "type": "int",
+            "default": 1,
+            "min": 1,
+            "help": "Total pixels in display."
         }]
     }
 ]
