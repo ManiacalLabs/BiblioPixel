96b04aaa31b972b302ce59af988aea8060db73ea
Tom Ritchford <tom@swirly.com>
2017-06-14 21:32:34 +0200

    Better error message for old Python versions.

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python3
 
+import platform
+
+MINIMUM_VERSION = '3.4.0'
+
+if platform.python_version() < MINIMUM_VERSION:
+    raise EnvironmentError(
+        'BiblioPixel needs Python %s or later: your Python version is %s' % (
+            MINIMUM_VERSION, platform.python_version()))
+
 from bibliopixel.main import main
 
 if __name__ == '__main__':
