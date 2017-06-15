3c98632b07cf90f24008b8ccd1dbe9ecebffd6c9
Tom Ritchford <tom@swirly.com>
2017-06-15 11:36:04 +0200

    Split off main/demo_table.py

--- bibliopixel/main/demo.py
+++ bibliopixel/main/demo.py
@@ -1,9 +1,7 @@
 import random, webbrowser
 
-from bibliopixel.drivers.SimPixel import SimPixel
-from bibliopixel import Circle, Matrix, layout
-from bibliopixel.animation import Sequence
-from bibliopixel.project import project
+from . demo_table import DEMO_TABLE
+from .. project import project
 
 HELP = """
 Run a demo.  For the list of possible demos, type
@@ -13,152 +11,7 @@ Run a demo.  For the list of possible demos, type
 """
 
 DEFAULT_SIMPIXEL_URL = 'http://simpixel.io'
-
-BLOOM = {
-    'driver': {
-        'typename': 'simpixel',
-        'num': 0
-    },
-
-    'led': {
-        'typename': 'matrix',
-        'width': 0,
-        'height': 0,
-    },
-
-    'animation': {
-        'typename': 'BiblioPixelAnimations.matrix.bloom.Bloom'
-    },
-}
-
-
-MATRIX_PROJECT = {
-    'driver': {
-        'typename': 'simpixel',
-        'num': 0
-    },
-
-    'led': {
-        'typename': 'matrix',
-        'width': 0,
-        'height': 0,
-    },
-
-    'animation': {
-        'typename': 'sequence',
-        'animations': [
-            {
-                'animation':
-                'BiblioPixelAnimations.matrix.MatrixRain.MatrixRainBow',
-                'run': {
-                    'amt': 1,
-                    'fps': 20,
-                    'seconds': 8,
-                },
-            },
-            {
-                'animation': {
-                    'typename': 'BiblioPixelAnimations.matrix.Text.ScrollText',
-                    'xPos': 16,
-                    'font_scale': 2,
-                    'text': 'BiblioPixel Demo'
-                },
-                'run': {
-                    'amt': 1,
-                    'fps': 30,
-                    'seconds': 8,
-                    'until_complete': True,
-                },
-            },
-            {
-                'animation': 'BiblioPixelAnimations.matrix.bloom.Bloom',
-                'run': {
-                    'amt': 3,
-                    'fps': 60,
-                    'seconds': 8,
-                },
-            },
-            {
-                'animation': 'BiblioPixelAnimations.matrix.circlepop.CirclePop',
-                'run': {
-                    'amt': 1,
-                    'fps': 30,
-                    'seconds': 8,
-                },
-            },
-        ],
-    },
-}
-
-CUBE_PROJECT = {
-    'driver': {
-        'typename': 'simpixel',
-        'num': 0
-    },
-
-    'led': {
-        'typename': 'cube',
-        'x': 0,
-        'y': 0,
-        'z': 0
-    },
-
-    'animation': {
-        'typename': 'sequence',
-        'animations': [
-            {
-                'animation': 'BiblioPixelAnimations.cube.Rain.RainBow',
-                'run': {
-                    'amt': 1,
-                    'fps': 10,
-                    'seconds': 8
-                },
-            },
-            {
-                'animation': 'BiblioPixelAnimations.cube.bloom.CubeBloom',
-                'run': {
-                    'amt': 6,
-                    'fps': 20,
-                    'seconds': 8,
-                },
-            }
-        ],
-    },
-}
-
-
-def matrix(args):
-    driver = SimPixel(args.width * args.height)
-    led = Matrix(driver, width=args.width, height=args.height)
-
-    anim = Sequence(led)
-
-    from BiblioPixelAnimations.matrix.MatrixRain import MatrixRainBow
-    anim.add_animation(MatrixRainBow(led), amt=1, fps=20, seconds=8)
-
-    from BiblioPixelAnimations.matrix.Text import ScrollText
-    anim.add_animation(
-        ScrollText(led, 'BiblioPixel Demo', xPos=args.width, font_scale=2),
-        amt=1, fps=30, until_complete=True)
-
-    from BiblioPixelAnimations.matrix.bloom import Bloom
-    anim.add_animation(Bloom(led), amt=3, fps=60, seconds=8)
-
-    from BiblioPixelAnimations.matrix.circlepop import CirclePop
-    anim.add_animation(CirclePop(led), amt=1, fps=30, seconds=8)
-
-    return anim
-
-
-def circle(args):
-    pixels_per = [1, 4, 8, 12, 18, 24, 32, 40, 52, 64]
-    rings, steps = layout.gen_circle(pixels_per=pixels_per)
-    points = layout.layout_from_rings(rings, origin=(200, 200, 0), z_diff=8)
-    driver = SimPixel(sum(pixels_per), layout=points)
-    led = Circle(driver, rings=rings, maxAngleDiff=0)
-
-    from BiblioPixelAnimations.circle.bloom import CircleBloom
-    return CircleBloom(led)
+DEMO_OPTS = ', '.join(sorted(DEMO_TABLE.keys()))
 
 
 def make_runnable(demo, args):
@@ -188,19 +41,8 @@ def make_runnable(demo, args):
     return project.project_to_runnable(demo)
 
 
-DEMO_TABLE = {
-    'bloom': BLOOM,
-    'circle': circle,
-    'matrix': matrix,
-    'cube': CUBE_PROJECT,
-    'matrix_project': MATRIX_PROJECT,
-}
-
-DEMO_OPTS = sorted(DEMO_TABLE.keys())
-
-
 def usage():
-    print('Available demos are: {}'.format(', '.join(DEMO_OPTS)))
+    print('Available demos are: {}'.format(DEMO_OPTS))
 
 
 def run(args, settings):
@@ -211,7 +53,7 @@ def run(args, settings):
     if not args.name:
         usage()
         args.name = random.choice(list(DEMO_TABLE))
-        print('Selected', args.name)
+        print('Randomly selected', args.name)
 
     try:
         demo = DEMO_TABLE[args.name]
@@ -228,13 +70,23 @@ def run(args, settings):
 def set_parser(parser):
     parser.set_defaults(run=run)
     parser.description = 'Run demo in SimPixel'
-    parser.add_argument('name', nargs='?', default='',
-                        help='Name of demo to run. Options are: {}'.format(', '.join(DEMO_OPTS)))
-    parser.add_argument('--width', default=16, type=int,
-                        help='X dimension of display')
-    parser.add_argument('--height', default=16, type=int,
-                        help='Y dimension of display')
-    parser.add_argument('--depth', default=16, type=int,
-                        help='Z dimension of dipslay. Only used for Cube demos.')
-    parser.add_argument('--simpixel', default=DEFAULT_SIMPIXEL_URL,
-                        help='URL of SimPixel to use if not http://SimPixel.io')
+
+    parser.add_argument(
+        'name', nargs='?', default='',
+        help='Name of demo to run. Options are: {}'.format(DEMO_OPTS))
+
+    parser.add_argument(
+        '--width', default=16, type=int,
+        help='X dimension of display')
+
+    parser.add_argument(
+        '--height', default=16, type=int,
+        help='Y dimension of display')
+
+    parser.add_argument(
+        '--depth', default=16, type=int,
+        help='Z dimension of display. Only used for Cube demos.')
+
+    parser.add_argument(
+        '--simpixel', default=DEFAULT_SIMPIXEL_URL,
+        help='URL for SimPixel program.')
---
+++ bibliopixel/main/demo_table.py
@@ -0,0 +1,158 @@
+from bibliopixel.drivers.SimPixel import SimPixel
+from bibliopixel import Circle, Matrix, layout
+from bibliopixel.animation import Sequence
+
+BLOOM = {
+    'driver': {
+        'typename': 'simpixel',
+        'num': 0
+    },
+
+    'led': {
+        'typename': 'matrix',
+        'width': 0,
+        'height': 0,
+    },
+
+    'animation': {
+        'typename': 'BiblioPixelAnimations.matrix.bloom.Bloom'
+    },
+}
+
+
+MATRIX_PROJECT = {
+    'driver': {
+        'typename': 'simpixel',
+        'num': 0
+    },
+
+    'led': {
+        'typename': 'matrix',
+        'width': 0,
+        'height': 0,
+    },
+
+    'animation': {
+        'typename': 'sequence',
+        'animations': [
+            {
+                'animation':
+                'BiblioPixelAnimations.matrix.MatrixRain.MatrixRainBow',
+                'run': {
+                    'amt': 1,
+                    'fps': 20,
+                    'seconds': 8,
+                },
+            },
+            {
+                'animation': {
+                    'typename': 'BiblioPixelAnimations.matrix.Text.ScrollText',
+                    'xPos': 16,
+                    'font_scale': 2,
+                    'text': 'BiblioPixel Demo'
+                },
+                'run': {
+                    'amt': 1,
+                    'fps': 30,
+                    'seconds': 8,
+                    'until_complete': True,
+                },
+            },
+            {
+                'animation': 'BiblioPixelAnimations.matrix.bloom.Bloom',
+                'run': {
+                    'amt': 3,
+                    'fps': 60,
+                    'seconds': 8,
+                },
+            },
+            {
+                'animation': 'BiblioPixelAnimations.matrix.circlepop.CirclePop',
+                'run': {
+                    'amt': 1,
+                    'fps': 30,
+                    'seconds': 8,
+                },
+            },
+        ],
+    },
+}
+
+CUBE_PROJECT = {
+    'driver': {
+        'typename': 'simpixel',
+        'num': 0
+    },
+
+    'led': {
+        'typename': 'cube',
+        'x': 0,
+        'y': 0,
+        'z': 0
+    },
+
+    'animation': {
+        'typename': 'sequence',
+        'animations': [
+            {
+                'animation': 'BiblioPixelAnimations.cube.Rain.RainBow',
+                'run': {
+                    'amt': 1,
+                    'fps': 10,
+                    'seconds': 8
+                },
+            },
+            {
+                'animation': 'BiblioPixelAnimations.cube.bloom.CubeBloom',
+                'run': {
+                    'amt': 6,
+                    'fps': 20,
+                    'seconds': 8,
+                },
+            }
+        ],
+    },
+}
+
+
+def matrix(args):
+    driver = SimPixel(args.width * args.height)
+    led = Matrix(driver, width=args.width, height=args.height)
+
+    anim = Sequence(led)
+
+    from BiblioPixelAnimations.matrix.MatrixRain import MatrixRainBow
+    anim.add_animation(MatrixRainBow(led), amt=1, fps=20, seconds=8)
+
+    from BiblioPixelAnimations.matrix.Text import ScrollText
+    anim.add_animation(
+        ScrollText(led, 'BiblioPixel Demo', xPos=args.width, font_scale=2),
+        amt=1, fps=30, until_complete=True)
+
+    from BiblioPixelAnimations.matrix.bloom import Bloom
+    anim.add_animation(Bloom(led), amt=3, fps=60, seconds=8)
+
+    from BiblioPixelAnimations.matrix.circlepop import CirclePop
+    anim.add_animation(CirclePop(led), amt=1, fps=30, seconds=8)
+
+    return anim
+
+
+def circle(args):
+    pixels_per = [1, 4, 8, 12, 18, 24, 32, 40, 52, 64]
+    rings, steps = layout.gen_circle(pixels_per=pixels_per)
+    points = layout.layout_from_rings(rings, origin=(200, 200, 0), z_diff=8)
+    driver = SimPixel(sum(pixels_per), layout=points)
+    led = Circle(driver, rings=rings, maxAngleDiff=0)
+
+    from BiblioPixelAnimations.circle.bloom import CircleBloom
+    return CircleBloom(led)
+
+
+DEMO_TABLE = {
+    'bloom': BLOOM,
+    'circle': circle,
+    'cube': CUBE_PROJECT,
+    'matrix': matrix,
+    'matrix_project': MATRIX_PROJECT,
+}
