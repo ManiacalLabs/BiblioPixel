88a06458ca23604a3911025ce66a0463f042219c
Tom Ritchford <tom@swirly.com>
2017-07-18 15:03:22 +0200

    Wait longer for process completion in sub_test.

--- test/sub_test.py
+++ test/sub_test.py
@@ -1,8 +1,11 @@
 import functools, time, unittest
-
 from bibliopixel.threads import sub
 
-pause = functools.partial(time.sleep, 0.005)
+WAIT_FOR_SUB = 0.1
+
+
+def pause(delay=0.005):
+    time.sleep(delay)
 
 
 def run(input, output, *arg, **kwds):
@@ -23,8 +26,8 @@ class SubTest(unittest.TestCase):
         self.assertTrue(s.is_alive())
         pause()
         self.assertTrue(s.is_alive())
-        pause()
-        pause()
+
+        pause(WAIT_FOR_SUB)
         self.assertFalse(s.is_alive())
 
     def test_subprocess(self):
