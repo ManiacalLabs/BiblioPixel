7acce18edac79068fe97629e8e054de120508240
Tom Ritchford <tom@swirly.com>
2017-07-31 19:11:25 +0200

    Remove startX and startY (fix #318)

--- bibliopixel/animation/matrix.py
+++ bibliopixel/animation/matrix.py
@@ -4,7 +4,7 @@ from .. layout import Matrix
 
 class BaseMatrixAnim(BaseAnimation):
 
-    def __init__(self, layout, width=0, height=0, startX=0, startY=0):
+    def __init__(self, layout, width=0, height=0):
         super().__init__(layout)
         if not isinstance(layout, Matrix):  # pragma: no cover
             raise RuntimeError('Must use bibliopixel.layout.Matrix with ' +
@@ -12,6 +12,3 @@ class BaseMatrixAnim(BaseAnimation):
 
         self.width = width or layout.width
         self.height = height or layout.height
-
-        self.startX = startX
-        self.startY = startY
