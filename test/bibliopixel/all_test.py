51427a99563f58f067b5c43988bc2586f406fd84
Tom Ritchford <tom@swirly.com>
2017-08-18 19:29:00 +0200

    Extract code from all_test.py

--- test/bibliopixel/all_test.py
+++ test/bibliopixel/all_test.py
@@ -1,42 +1,11 @@
-import importlib, os, unittest
+import os, unittest
+from . import_all import import_all
 
 BLACKLIST = ['bibliopixel.drivers.PiWS281X']
-
-
-def split_all(path):
-    result = []
-    old_path = None
-    while path != old_path:
-        (path, tail), old_path = os.path.split(path), path
-        tail and result.insert(0, tail)
-    return result
-
-
-def get_imports():
-    bp_root = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-    python_root = os.path.join(bp_root, 'bibliopixel')
-    for directory, sub_folders, files in os.walk(python_root):
-        if '__' in directory:
-            continue
-
-        relative = os.path.relpath(directory, bp_root)
-        root_import = '.'.join(split_all(relative))
-
-        yield root_import
-
-        for f in files:
-            if f.endswith('.py') and '__' not in f:
-                yield '%s.%s' % (root_import, f[:-3])
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 
 class ImportAllTest(unittest.TestCase):
     def test_all(self):
-        failures = []
-        for name in get_imports():
-            if name not in BLACKLIST:
-                try:
-                    importlib.import_module(name)
-                except:
-                    failures.append(name)
-
+        _, failures = import_all(PROJECT_ROOT, 'bibliopixel', BLACKLIST)
         self.assertEqual(failures, [])
---
+++ test/bibliopixel/import_all.py
@@ -0,0 +1,41 @@
+import importlib, os
+
+
+def _split_all(path):
+    result = []
+    old_path = None
+    while path != old_path:
+        (path, tail), old_path = os.path.split(path), path
+        tail and result.insert(0, tail)
+    return result
+
+
+def _all_imports(root, project_name):
+    python_root = os.path.join(root, project_name)
+    for directory, sub_folders, files in os.walk(python_root):
+        if '__' in directory:
+            continue
+
+        relative = os.path.relpath(directory, root)
+        root_import = '.'.join(_split_all(relative))
+
+        yield root_import
+
+        for f in files:
+            if f.endswith('.py') and '__' not in f:
+                yield '%s.%s' % (root_import, f[:-3])
+
+
+def import_all(root, project_name, blacklist):
+    """Import all files and directories """
+    successes, failures = [], []
+    for name in _all_imports(root, project_name):
+        if name not in blacklist:
+            try:
+                importlib.import_module(name)
+            except Exception as e:
+                failures.append((name, e))
+            else:
+                successes.append(name)
+
+    return successes, failures
