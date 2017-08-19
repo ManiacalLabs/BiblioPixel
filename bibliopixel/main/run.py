af84a87a55a079f8b3a911f684c229347727da41
Tom Ritchford <tom@swirly.com>
2017-08-19 02:01:33 +0200

    Now bp run accepts more than one project.

--- bibliopixel/main/run.py
+++ bibliopixel/main/run.py
@@ -6,26 +6,50 @@ import loady, json, os, time
 from . import common_flags, simpixel
 from .. util import log
 
+RUN_ERROR = """When reading description:
+{desc}
+encounted exception
+{exception}
+"""
+
+FAILURE_ERROR = '{count} project{s} failed'
+
 
 def run(args):
     common_flags.extend_path(args)
-    if args.json:
-        desc = args.name
-    else:
-        desc = args.name and loady.data.load(args.name, True)
 
-    animation = common_flags.make_animation(args, desc or {})
+    animations, failed = [], []
+
+    for name in args.name or ['']:
+        if args.json:
+            desc = name
+        else:
+            desc = name and loady.data.load(name, True)
+
+        try:
+           animations.append(common_flags.make_animation(args, desc or {}))
+        except Exception as exception:
+            failed.append(RUN_ERROR.format(**locals()))
+
+    if failed:
+        log.error(FAILURE_ERROR.format(
+            count=len(failed), s='' if len(failed) == 1 else 's'))
+        for f in failed:
+            log.error(f)
+        raise ValueError('Run aborted')
 
     if args.simpixel:
         simpixel.open_simpixel(args.simpixel)
     elif args.s:
         simpixel.open_simpixel()
 
-    try:
-        animation.layout.start()
-        animation.start()
-    except KeyboardInterrupt:
-        print('\nTermination requested by user.')
+    for animation in animations:
+        try:
+            animation.layout.start()
+            animation.start()
+        except KeyboardInterrupt:
+            log.warning('\nTermination requested by user.')
+
         animation.cleanup()
         animation.layout.cleanup_drivers()
 
@@ -37,9 +61,8 @@ def set_parser(parser):
     common_flags.add_project_flags(parser)
 
     parser.add_argument(
-        'name', nargs='?',
-        help='Path project file - can be a URL or file system location',
-        default='')
+        'name', nargs='*',
+        help='Path project files - can be a URL or file system location')
 
     parser.add_argument(
         '-j', '--json', action='store_true',
