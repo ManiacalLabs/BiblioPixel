d76f2838085bd754d81009f7c95c721e399d9924
Tom Ritchford <tom@swirly.com>
2017-08-24 13:18:13 +0200

    Change sub_test to use its queues.

--- test/bibliopixel/threads/sub_test.py
+++ test/bibliopixel/threads/sub_test.py
@@ -4,12 +4,14 @@ from bibliopixel.threads import sub
 WAIT_FOR_SUB = 0.1
 
 
-def pause(delay=0.005):
+def pause(delay=0.01):
     time.sleep(delay)
 
 
 def run(input, output, *arg, **kwds):
     pause()
+
+    output.put('first')
 
     if arg != (1, 2, 3):
         raise ValueError('1 2 3')
@@ -18,16 +20,18 @@ def run(input, output, *arg, **kwds):
         raise ValueError('a=1')
 
     pause()
+    output.put('second')
 
 
 class SubTest(unittest.TestCase):
     def do_test(self, use_subprocess):
-        s, i, o = sub.run(run, 1, 2, 3, a=1, use_subprocess=use_subprocess)
+        s, input, output = sub.run(
+            run, 1, 2, 3, a=1, use_subprocess=use_subprocess)
         self.assertTrue(s.is_alive())
+        self.assertEqual(output.get(), 'first')
+        self.assertTrue(s.is_alive())
+        self.assertEqual(output.get(), 'second')
         pause()
-        self.assertTrue(s.is_alive())
-
-        pause(WAIT_FOR_SUB)
         self.assertFalse(s.is_alive())
 
     def test_subprocess(self):
