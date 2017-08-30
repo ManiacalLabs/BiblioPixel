ea2f5fffbac1e0bbd90baf5a54d317548c4949b6
Tom Ritchford <tom@swirly.com>
2017-08-30 16:53:59 +0200

    ColorList encapsulates numpy or Python lists of color.

--- bibliopixel/util/color_list.py
+++ bibliopixel/util/color_list.py
@@ -1,8 +1,74 @@
-import functools
+import copy
+
 try:
     import numpy
-    numpy_array = numpy.array
+    numpy_array = numpy.ndarray
 
 except:
     numpy = None
     numpy_array = ()
+
+
+class Base:
+    def __init__(self, color_list):
+        self.color_list = color_list
+
+    def __len__(self):
+        return len(self.color_list)
+
+    def __getitem__(self, i):
+        return self.color_list[i]
+
+
+class List(Base):
+    def clear(self):
+        if self.color_list:
+            black = tuple(0 for i in self[0])
+            self.color_list[:] = (black for i in self.color_list)
+
+    def add(self, source, level):
+        def add(colors):
+            return tuple(int(c + level * s) for (c, s) in zip(*colors))
+
+        if level:
+            self.color_list[:] = map(add, zip(self.color_list, source))
+
+    def copy_from(self, source):
+        self.color_list[:] = source
+
+
+class Numpy(Base):
+    _buffer = None
+
+    def clear(self):
+        self.color_list.fill(0)
+
+    def add(self, source, level):
+        if level:
+            self._buffer = numpy.multiply(
+                level, source, out=self._buffer, casting='unsafe')
+            print(self._buffer)
+            self.color_list += self._buffer
+
+    def copy_from(self, source):
+        numpy.copyto(self.color_list, source, casting='unsafe')
+
+
+def ColorList(color_list):
+    maker = Numpy if isinstance(color_list, numpy_array) else List
+    return maker(color_list)
+
+
+class Mixer:
+    def __init__(self, color_list, sources, levels=None):
+        self.color_list = ColorList(color_list)
+        self.sources = sources
+
+        cl = len(self.sources)
+        padded_levels = (levels or []) + [0] * cl
+        self.levels = padded_levels[:cl]
+        self.clear = self.color_list.clear
+
+    def mix(self):
+        for source, level in zip(self.sources, self.levels):
+            self.color_list.add(source, level)
---
+++ test/bibliopixel/util/color_list_test.py
@@ -0,0 +1,148 @@
+import numpy, unittest
+from numpy import array_equal
+
+from bibliopixel.util import color_list
+from bibliopixel import colors
+
+COLORS1 = [colors.Red, colors.Green, colors.Blue, colors.White]
+COLORS2 = [colors.Black, colors.Blue, colors.Red, colors.Black]
+SUM12 = [colors.Red, colors.Cyan, colors.Magenta, colors.White]
+WHITES = [colors.White, colors.White, colors.White, colors.White]
+BLACKS = [colors.Black, colors.Black, colors.Black, colors.Black]
+
+
+def array(cl):
+    return numpy.array(cl, dtype='uint8')
+
+
+def make_list(cl):
+    return color_list.ColorList(cl[:])
+
+
+def make_numpy(cl):
+    return color_list.ColorList(array(cl))
+
+
+class TestBase(unittest.TestCase):
+    def assert_list_equal(self, actual, expected):
+        x, y = actual.color_list, expected
+        if isinstance(x, color_list.numpy_array):
+            equals = array_equal(x, array(y))
+        else:
+            equals = (x == y)
+        if not equals:
+            print('____')
+            print(x)
+            print('NOT EQUAL')
+            print(y)
+            print('____')
+        self.assertTrue(equals)
+
+
+class ColorListTest(TestBase):
+    def assert_list_equal(self, actual, expected):
+        x, y = actual.color_list, expected
+        if isinstance(x, color_list.numpy_array):
+            equals = array_equal(x, array(y))
+        else:
+            equals = (x == y)
+        if not equals:
+            print('NOT EQUAL')
+            print(x)
+            print(y)
+        self.assertTrue(equals)
+
+    def test_simple(self):
+        self.assertIs(numpy, color_list.numpy)
+        self.assertIs(numpy.ndarray, color_list.numpy_array)
+
+        cl = make_list([])
+        self.assertIsInstance(cl, color_list.List)
+
+        cl = make_numpy([])
+        self.assertIsInstance(cl, color_list.Numpy)
+
+    def test_lists(self):
+        cl1 = make_list(COLORS1)
+        cl2 = make_list(COLORS2)
+        cl1.add(cl2.color_list, 0)
+        self.assert_list_equal(cl1, COLORS1)
+        cl1.add(cl2.color_list, 1)
+        self.assert_list_equal(cl1, SUM12)
+
+    def test_numpy(self):
+        cl1 = make_numpy(COLORS1)
+        cl2 = make_numpy(COLORS2)
+        cl1.add(cl2.color_list, 0)
+        self.assert_list_equal(cl1, COLORS1)
+        cl1.add(cl2.color_list, 1)
+        self.assertTrue(cl1, SUM12)
+
+    def test_clear_list(self):
+        cl = make_list(COLORS1)
+        cl.clear()
+        self.assert_list_equal(cl, BLACKS)
+
+    def test_clear_numpy(self):
+        cl = make_numpy(COLORS1)
+        cl.clear()
+        self.assert_list_equal(cl, BLACKS)
+
+    def test_copy_list(self):
+        cl = make_list(COLORS1)
+        cl = make_numpy(COLORS1)
+        cl.copy_from(array(COLORS2))
+        self.assert_list_equal(cl, COLORS2)
+
+    def test_copy_numpy(self):
+        cl = make_numpy(COLORS1)
+        cl.copy_from(array(COLORS2))
+        self.assert_list_equal(cl, COLORS2)
+
+
+class MixerTest(TestBase):
+    def do_test(self, mixer, thirds):
+        self.assertEqual(mixer.levels, [0, 0, 0])
+        self.assert_list_equal(mixer.color_list, COLORS1)
+
+        mixer.mix()
+        self.assert_list_equal(mixer.color_list, COLORS1)
+
+        mixer.clear()
+        mixer.mix()
+
+        self.assert_list_equal(mixer.color_list, BLACKS)
+
+        mixer.levels[:] = [1, 0, 0]
+        mixer.clear()
+        mixer.mix()
+        self.assert_list_equal(mixer.color_list, COLORS2)
+
+        mixer.levels[:] = [0, 1, 0]
+        mixer.clear()
+        mixer.mix()
+        self.assert_list_equal(mixer.color_list, WHITES)
+
+        mixer.levels[:] = [0, 0, 1]
+        mixer.clear()
+        mixer.mix()
+        self.assert_list_equal(mixer.color_list, BLACKS)
+
+        mixer.levels[:] = [1 / 3, 1 / 3, 1 / 3]
+        mixer.clear()
+        mixer.mix()
+        self.assert_list_equal(mixer.color_list, thirds)
+
+    def test_lists(self):
+        mixer = color_list.Mixer(COLORS1[:], [COLORS2, WHITES, BLACKS])
+        self.do_test(mixer,
+                     [(85, 85, 85), (85, 85, 170), (170, 85, 85), (85, 85, 85)])
+
+    def test_numpy(self):
+        mixer = color_list.Mixer(
+            array(COLORS1), [array(COLORS2), array(WHITES), array(BLACKS)])
+        self.do_test(mixer,
+                     [(85, 85, 85), (85, 85, 170), (170, 85, 85), (85, 85, 85)])
+
+
+del TestBase
