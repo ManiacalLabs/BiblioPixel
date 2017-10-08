3e80dc9494d8f5d7e4774a8ddd54453929fa84a2
Tom Ritchford <tom@swirly.com>
2017-10-08 17:22:24 +0200

    layout.Cutter cuts up layout.Matrix

---
+++ bibliopixel/layout/cutter.py
@@ -0,0 +1,69 @@
+"""
+Cut matrices by row or column and apply operations to them.
+"""
+
+
+class Cutter:
+    """
+    Base class that pre-calculates cuts and can use them to
+    apply a function to the layout.
+
+    Each "cut" is a row or column, depending on the value of by_row.
+
+    The entries are iterated forward or backwards, depending on the
+    value of forward.
+    """
+    def __init__(self, layout, by_row=True):
+        self.layout = layout
+        cuts = layout.height if by_row else layout.width
+        cutter = self.cut_row if by_row else self.cut_column
+        self.cuts = [cutter(i) for i in range(cuts)]
+
+    def apply(self, function):
+        """
+        For each row or column in cuts, read a list of its colors,
+        apply the function to that list of colors, then write it back
+        to the layout.
+        """
+        for cut in self.cuts:
+            value = self.read(cut)
+            function(value)
+            self.write(cut, value)
+
+
+class Slicer(Cutter):
+    """
+    Implementation of Cutter that uses slices of the underlying colorlist.
+    Does not work if the Matrix layout is serpentine or has any reflections
+    or rotations.
+    """
+    def cut_row(self, i):
+        return slice(self.layout.width * i, self.layout.width * (i + 1))
+
+    def cut_column(self, i):
+        return slice(i, None, self.layout.width)
+
+    def read(self, cut):
+        return self.layout.color_list[cut]
+
+    def write(self, cut, value):
+        self.layout.color_list[cut] = value
+
+
+class Indexer(Cutter):
+    """
+    Slower implementation of Cutter that uses lists of indices and the
+    Matrix interface.
+    """
+    def cut_row(self, i):
+        return [(column, i) for column in range(self.layout.width)]
+
+    def cut_column(self, i):
+        return [(i, row) for row in range(self.layout.height)]
+
+    def read(self, cut):
+        return [self.layout.get(*i) for i in cut]
+
+    def write(self, cut, value):
+        for i, v in zip(cut, value):
+            self.layout.set(*i, color=v)
---
+++ test/bibliopixel/layout/cutter_test.py
@@ -0,0 +1,50 @@
+import unittest
+from bibliopixel.layout import cutter
+
+
+class MockLayout:
+    def __init__(self, color_list, width, height):
+        self.results = []
+        self.color_list = list(color_list)
+        self.width = width
+        self.height = height
+
+    def get(self, column, row):
+        return self.color_list[self.width * row + column]
+
+    def set(self, column, row, color):
+        self.color_list[column + self.width * row] = color
+
+
+class CutterTest(unittest.TestCase):
+    def assert_cut(self, expected_slices, expected_result,
+                   by_row=True, width=3, height=2, color_list='abcdef'):
+        layout = MockLayout(color_list, width, height)
+        cutter = self.maker(layout, by_row)
+        slices = []
+
+        def function(value):
+            slices.append(''.join(value))
+            value.append(value.pop(0))
+
+        cutter.apply(function)
+        result = ''.join(layout.color_list)
+        self.assertEquals(expected_slices, slices)
+        self.assertEquals(expected_result, result)
+
+    def test_get_row(self):
+        self.assert_cut(['abc', 'def'], 'bcaefd')
+
+    def test_get_column(self):
+        self.assert_cut(['ad', 'be', 'cf'], 'defabc', by_row=False)
+
+
+class SlicerTest(CutterTest):
+    maker = cutter.Slicer
+
+
+class IndexerTest(CutterTest):
+    maker = cutter.Indexer
+
+
+del CutterTest
