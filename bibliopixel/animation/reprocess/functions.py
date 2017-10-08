9901fd30f23e2d81f5a215cf506a13456211e512
Tom Ritchford <tom@swirly.com>
2017-10-08 17:22:23 +0200

    Reprocess function 'sorter'

---
+++ bibliopixel/animation/reprocess/functions.py
@@ -0,0 +1,17 @@
+from ... colors.conversions import color_cmp
+
+
+def sorter(x):
+    if len(x) < 2:
+        return
+
+    prev = x[0]
+    for i in range(1, len(x)):
+        next = x[i]
+        if color_cmp(prev, next) > 0:
+            x[i - 1], x[i] = next, prev
+            return
+        prev = next
+
+
+FUNCTIONS = {'sorter': sorter}
---
+++ test/bibliopixel/animation/reprocess/sorter_test.py
@@ -0,0 +1,62 @@
+import unittest
+from bibliopixel.colors.conversions import hsv2rgb_raw
+from bibliopixel.animation.reprocess import functions
+
+# Map letters to colors so that we can read inputs and outputs.
+COLORS = {ch: hsv2rgb_raw((ord(ch), 127, 127)) for ch in 'abcdef'}
+INVERSE_COLORS = {c: ch for (ch, c) in COLORS.items()}
+
+
+def sorter(input):
+    s = [COLORS[i] for i in input]
+    functions.sorter(s)
+    return ''.join(INVERSE_COLORS[c] for c in s)
+
+
+class SorterTest(unittest.TestCase):
+    def test_empty(self):
+        self.assertEquals(sorter(''), '')
+
+    def test_one(self):
+        self.assertEquals(sorter('a'), 'a')
+
+    def test_one(self):
+        self.assertEquals(sorter('ba'), 'ab')
+
+    def test_sorted(self):
+        self.assertEquals(sorter('a'), 'a')
+        self.assertEquals(sorter('ab'), 'ab')
+        self.assertEquals(sorter('abc'), 'abc')
+        self.assertEquals(sorter('abcd'), 'abcd')
+        self.assertEquals(sorter('abcde'), 'abcde')
+
+    def test_reverse(self):
+        results = ['fedcba']
+
+        while True:
+            prev = results[-1]
+            next = sorter(prev)
+            if next == prev:
+                break
+            results.append(next)
+
+        expected = [
+            'fedcba',
+            'efdcba',
+            'edfcba',
+            'defcba',
+            'decfba',
+            'dcefba',
+            'cdefba',
+            'cdebfa',
+            'cdbefa',
+            'cbdefa',
+            'bcdefa',
+            'bcdeaf',
+            'bcdaef',
+            'bcadef',
+            'bacdef',
+            'abcdef',
+        ]
+
+        self.assertEquals(results, expected)
