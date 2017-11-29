a5a1ba8d543c1e2ebd79918b5d3b20790b5726d9
Tom Ritchford <tom@swirly.com>
2017-11-29 13:07:29 +0100

    extractor.py extracts fields from incoming messages.

---
+++ bibliopixel/controllers/extractor.py
@@ -0,0 +1,73 @@
+import collections
+
+
+class Extractor:
+    """
+    MessageToDict is a class that extracts and normalizes values from
+    incoming message dictionaries into ordered dictionaries based on the
+    `type` key of each message.
+    """
+
+    def __init__(self, omit=None, normalizers=None, keys_by_type=None,
+                 accept=None, reject=None, auto_omit=True):
+        """
+        Arguments
+
+        omit -- A list of keys that will not be extracted.
+
+        normalizers -- Some keys also need to be "normalized" -
+            scaled and offset so they are between 0 and 1, or -1 and 1.
+            The `normalizers` table maps key names to a function that
+            normalizes the value of that key.
+
+        keys_by_type -- `keys_by_type` is a dictionary from the `type` in an
+            incoming message to a list of message keys to be extracted
+
+        accept -- maps keys to a value or a list of values that are
+            accepted for that key.  A message has to match *all* entries in
+            `accept` to be accepted.
+
+        reject -- map key to a value or a list of values that are not
+            accepted for that key.  A message is rejected if it matches *any*
+            entry in the reject map.
+
+        auto_omit -- if True, omit all keys in `accept` that only have one
+            possible value.
+
+            auto_omit=True, the default, is probably more useful: if you
+            request data for channel=1, type=note_on then you probably don't
+            want to see channel=1, type=note_on with each message.
+        """
+        def to_set(x):
+            if x is None:
+                return set()
+            if isinstance(x, (list, tuple)):
+                return set(x)
+            return set([x])
+
+        def make_match(m):
+            return m and {k: to_set(v) for k, v in m.items()}
+
+        self.accept, self.reject = make_match(accept), make_match(reject)
+        omit = to_set(omit)
+        if auto_omit and self.accept:
+            omit.update(k for k, v in self.accept.items() if len(v) == 1)
+
+        self.normalizers = normalizers or {}
+        self.keys_by_type = {}
+        for k, v in (keys_by_type or {}).items():
+            self.keys_by_type[k] = tuple(i for i in v if i not in omit)
+
+    def extract(self, msg):
+        """Yield an ordered dictionary if msg['type'] is in keys_by_type."""
+        def normal(key):
+            v = msg[key]
+            normalizer = self.normalizers.get(key)
+            return normalizer(v) if (normalizer and v is not None) else v
+
+        def match(m):
+            return (msg.get(k) in v for k, v in m.items()) if m else ()
+
+        keys = self.keys_by_type.get(msg.get('type'))
+        if keys and all(match(self.accept)) and not any(match(self.reject)):
+            yield collections.OrderedDict((k, normal(k)) for k in keys)
---
+++ test/bibliopixel/controllers/extractor_test.py
@@ -0,0 +1,54 @@
+import unittest
+from bibliopixel.controllers import extractor
+
+KEYS_BY_TYPE = {
+    'note_on': ('channel', 'type', 'note', 'velocity'),
+    'control_change': ('channel', 'type', 'control', 'value'),
+    'pitchwheel': ('channel', 'type', 'pitch'),
+}
+
+NORMALIZERS = {
+    'pitch': lambda x: (x - 8192) / 8192,
+    'value': lambda x: x / 127,
+    'velocity': lambda x: x / 127,
+}
+
+C3 = {'type': 'note_on', 'note': 32, 'channel': 1, 'velocity': 96}
+C3_OFF = {'type': 'note_off', 'note': 32, 'channel': 1, 'velocity': 0, 'x': 47}
+
+BC = {'type': 'control_change', 'channel': 2, 'control': 2, 'value': 10}
+BC3 = {'type': 'control_change', 'channel': 3, 'control': 2, 'value': 128}
+MOD = {'type': 'control_change', 'channel': 2, 'control': 1, 'value': 128}
+PB = {'type': 'control_change', 'channel': 2, 'control': 1, 'value': 128}
+
+OTHER = {'type': 'other', 'channel': 32, 'thing': 'stuff'}
+
+
+class ExtractorTest(unittest.TestCase):
+    def run_test(self, msg, expected, **kwds):
+        md = extractor.Extractor(
+            keys_by_type=KEYS_BY_TYPE,
+            normalizers=NORMALIZERS, **kwds)
+
+        actual = list(md.extract(msg))
+        if expected is None:
+            self.assertEquals(actual, [])
+        else:
+            self.assertEquals(list(actual[0].items()), expected)
+
+    def test_one(self):
+        expected = [
+            ('channel', 1),
+            ('type', 'note_on'),
+            ('note', 32),
+            ('velocity', 96 / 127)]
+
+        self.run_test(C3, expected)
+        self.run_test(C3, expected[1:], omit='channel')
+
+    def test_accept(self):
+        accept = {'channel': 2, 'type': 'control_change', 'control': 2}
+        for msg in C3, C3_OFF, BC3, MOD, OTHER:
+            self.run_test(msg, None, accept=accept)
+
+        self.run_test(BC, [('value', 10 / 127)], accept=accept)
