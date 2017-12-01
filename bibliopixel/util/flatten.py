5773c675d5056a18888cab1a4e52b2254bc37d3d
Tom Ritchford <tom@swirly.com>
2017-12-01 12:32:37 +0100

    Unflatten unpacks flat arrays into deep ones.
    * The inverse of flatten

--- bibliopixel/util/flatten.py
+++ bibliopixel/util/flatten.py
@@ -42,3 +42,40 @@ def flatten(master):
 
     recurse(master)
     return result
+
+
+def unflatten(master):
+    """
+    Unflattens a single-level dictionary a multilevel into one so that::
+
+        {'foo.bar.a': 1,
+         'foo.bar.b': True,
+         'foo.bar.a': 1,
+         }
+
+    would become::
+
+        {'foo':
+            {'bar':
+               {
+                   'a': 1,
+                   'b': True,
+                   'c': 'hello',
+                },
+            },
+        }
+    """
+    result = {}
+
+    for k, v in master.items():
+        *first, last = k.split('.')
+        r = result
+        for i in first:
+            r = r.setdefault(i, {})
+        r[last] = v
+
+    return result
+
+
+def canonical(master):
+    return unflatten(flatten(master))
--- test/bibliopixel/util/flatten_test.py
+++ test/bibliopixel/util/flatten_test.py
@@ -1,28 +1,41 @@
 import unittest
 
-from bibliopixel.util.flatten import flatten
+from bibliopixel.util.flatten import canonical, flatten, unflatten
 
 
 class FlattenTest(unittest.TestCase):
     def assert_flatten(self, before, after=None):
         self.assertEqual(flatten(before), after or before)
         self.assertEqual(flatten(flatten(before)), after or before)
+        after and self.assertEqual(canonical(before), canonical(after))
+
+    def assert_unflatten(self, before, after=None):
+        self.assertEqual(before, unflatten(after or before))
+        self.assertEqual(before, unflatten(unflatten(after or before)))
+        after and self.assertEqual(canonical(before), canonical(after))
 
     def test_trivial(self):
         self.assert_flatten({})
 
     def test_simple(self):
-        self.assert_flatten({'a': 1, 'c.d': True})
+        before = {'a': 1, 'c': {'d': True}}
+        after = {'a': 1, 'c.d': True}
+        self.assert_flatten(before, after)
+        self.assert_unflatten(before, after)
 
     def test_three_levels(self):
         before = {'foo': {'bar': {'a': 1, 'b': True, 'c': 'X'}}}
         after = {'foo.bar.a': 1, 'foo.bar.b': True, 'foo.bar.c': 'X'}
         self.assert_flatten(before, after)
+        self.assert_unflatten(before, after)
 
     def test_mix(self):
         before = {'foo': {'bar': {'a': 1}, 'bar.c': 'X'}, 'foo.bar.b': True}
         after = {'foo.bar.a': 1, 'foo.bar.b': True, 'foo.bar.c': 'X'}
+        canon = {'foo': {'bar': {'a': 1, 'b': True, 'c': 'X'}}}
         self.assert_flatten(before, after)
+        self.assert_unflatten(canon, after)
+        self.assertEqual(canonical(before), canon)
 
     def test_error(self):
         with self.assertRaises(ValueError):
