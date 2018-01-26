648563c00bcc41db6864de153c5198029288081e
Tom Ritchford <tom@swirly.com>
2018-01-26 13:26:25 +0100

    Better text for `bp --help`
    * Fixed `bp -h` to give the same results as `bp --help`

--- bibliopixel/main/main.py
+++ bibliopixel/main/main.py
@@ -6,10 +6,12 @@ from .. project import aliases, project
 
 
 __all__ = ['main']
+
 COMMANDS = (
     'all_pixel', 'all_pixel_test', 'clear_cache', 'color', 'demo',
     'devices', 'info', 'load', 'list', 'remove', 'reset', 'run', 'save', 'set',
     'show', 'update')
+
 MODULES = {c: import_module('bibliopixel.main.' + c) for c in COMMANDS}
 
 
@@ -20,11 +22,11 @@ def no_command(*_):
 
 
 def get_args(argv=sys.argv):
-    argv = argv[1:]
+    argv = ['-h' if a == '--help' else a for a in argv[1:]]
 
     # argparse doesn't give command-specific help for `bp --help <command>`
     # so we use `bp <command> --help` (#429)
-    if len(argv) == 2 and argv[0] == '--help':
+    if len(argv) == 2 and argv[0] == '-h':
         argv.reverse()
 
     try:
@@ -36,7 +38,7 @@ def get_args(argv=sys.argv):
         if not argv:
             return
 
-    if argv and not argv[0].isidentifier() and '--help' not in argv:
+    if argv and not argv[0].isidentifier() and '-h' not in argv:
         # The first argument can't be a command so try to run it.
         argv.insert(0, 'run')
 
@@ -55,6 +57,8 @@ def get_args(argv=sys.argv):
         module.set_parser(subparser)
         subparser.description = doc + getattr(module, 'DESCRIPTION', '')
 
+    if argv == ['-h']:
+        log.printer(HELP)
     return parser.parse_args(argv)
 
 
@@ -86,3 +90,60 @@ def main():
         result = getattr(e, 'errorcode', -1)
 
     sys.exit(result)
+
+
+HELP = """\
+bp - The BiblioPixel Project Runner.
+
+`bp` is a command-line script installed with Bibliopixel.  It can run projects
+and demos, configure hardware, save and load defaults, and more.
+
+`bp` takes one of {0} *commands*:
+  {1}
+  {2}
+
+Some commands take *arguments* - often names of files.
+
+Examples of `bp` commands and arguments:
+
+    bp info
+    bp demo circle
+    bp run foo.json bar.json
+    bp foo.json bar.json      # run is the default command
+
+The commands are summarized below, and you can get more detailed help about
+each one by typing:
+
+    bp <command> --help
+
+or
+
+    bp <command> -h
+
+`bp` commands have *flags* that control how `bp` runs its commands.
+
+Examples of flags are -v, --verbose, --defaults, -d, --simpixel.  Flags either
+start with -, for one letter flags, or -- for multi-letter flags.
+
+Sometimes two flags have the same meaning, like --simpixel and -s,
+--verbose and -v, or --dimensions and --dim.
+
+Some flags take an argument, like --dim=160 or --loglevel=frame.
+Other flags do not, like --verbose or -v.
+
+A `bp` command line can optionally include a command, a list of arguments,
+and a list of flags:
+
+    bp [<command>] [<argument> ...] [<flag> ...]
+
+Examples of `bp` command lines:
+
+    bp info --help
+    bp run -h
+    bp info
+    bp color "indian red 4"
+    bp foo.json bar.json -v
+    bp foo.json bar.json -v --numbers=float --dump --defaults=default.json
+""".format(len(COMMANDS),
+           ', '.join(COMMANDS[0:8]),
+           ', '.join(COMMANDS[8:]))
