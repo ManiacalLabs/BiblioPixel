1bf308256af5455a7b4d14d3d026b8dbf71404e9
Tom Ritchford <tom@swirly.com>
2018-02-01 16:27:06 +0100

    Split main/commands.py off main/main.py

--- bibliopixel/main/main.py
+++ bibliopixel/main/main.py
@@ -1,24 +1,9 @@
 import argparse, os, sys
-from . import common_flags
+from . import commands, common_flags
 from .. util import log
-from .. project.importer import import_module
 from .. project import aliases, project
 
-
 __all__ = ['main']
-
-COMMANDS = (
-    'all_pixel', 'all_pixel_test', 'clear_cache', 'color', 'demo',
-    'devices', 'info', 'load', 'list', 'remove', 'reset', 'run', 'save', 'set',
-    'show', 'update')
-
-MODULES = {c: import_module('bibliopixel.main.' + c) for c in COMMANDS}
-
-
-def no_command(*_):
-    log.printer('ERROR: No command entered')
-    log.printer('Valid:', ', '.join(COMMANDS))
-    return -1
 
 
 def get_args(argv=sys.argv):
@@ -50,7 +35,7 @@ def get_args(argv=sys.argv):
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers()
 
-    for name, module in sorted(MODULES.items()):
+    for name, module in sorted(commands.MODULES.items()):
         doc = module.__doc__
         subparser = subparsers.add_parser(name, help=doc)
         common_flags.add_common_flags(subparser)
@@ -58,7 +43,7 @@ def get_args(argv=sys.argv):
         subparser.description = doc + getattr(module, 'DESCRIPTION', '')
 
     if argv == ['-h']:
-        log.printer(HELP)
+        log.printer(commands.HELP)
     return parser.parse_args(argv)
 
 
@@ -68,7 +53,7 @@ def main():
     if not run:
         log.printer('ERROR: No command entered')
         log.printer('Valid commands are:')
-        log.printer('    ', ', '.join(COMMANDS))
+        log.printer('    ', ', '.join(commands.COMMANDS))
         log.printer()
         log.printer('For more help, type')
         log.printer()
@@ -90,60 +75,3 @@ def main():
         result = getattr(e, 'errorcode', -1)
 
     sys.exit(result)
-
-
-HELP = """\
-bp - The BiblioPixel Project Runner.
-
-`bp` is a command-line script installed with Bibliopixel.  It can run projects
-and demos, configure hardware, save and load defaults, and more.
-
-`bp` takes one of {0} *commands*:
-  {1}
-  {2}
-
-Some commands take *arguments* - often names of files.
-
-Examples of `bp` commands and arguments:
-
-    bp info
-    bp demo circle
-    bp run foo.json bar.json
-    bp foo.json bar.json      # run is the default command
-
-The commands are summarized below, and you can get more detailed help about
-each one by typing:
-
-    bp <command> --help
-
-or
-
-    bp <command> -h
-
-`bp` commands have *flags* that control how `bp` runs its commands.
-
-Examples of flags are -v, --verbose, --defaults, -d, --simpixel.  Flags either
-start with -, for one letter flags, or -- for multi-letter flags.
-
-Sometimes two flags have the same meaning, like --simpixel and -s,
---verbose and -v, or --dimensions and --dim.
-
-Some flags take an argument, like --dim=160 or --loglevel=frame.
-Other flags do not, like --verbose or -v.
-
-A `bp` command line can optionally include a command, a list of arguments,
-and a list of flags:
-
-    bp [<command>] [<argument> ...] [<flag> ...]
-
-Examples of `bp` command lines:
-
-    bp info --help
-    bp run -h
-    bp info
-    bp color "indian red 4"
-    bp foo.json bar.json -v
-    bp foo.json bar.json -v --numbers=float --dump --defaults=default.json
-""".format(len(COMMANDS),
-           ', '.join(COMMANDS[0:8]),
-           ', '.join(COMMANDS[8:]))
