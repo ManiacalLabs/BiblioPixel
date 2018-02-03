82f1949abe4d9c568caf91af0aecfddd36a39ee6
Tom Ritchford <tom@swirly.com>
2018-02-03 20:19:08 +0100

    Document util/threads/sub.py; omit some files from Sphinx

--- bibliopixel/util/threads/sub.py
+++ bibliopixel/util/threads/sub.py
@@ -13,19 +13,17 @@ def run(function, *args, use_subprocess=False, daemon=True, **kwds):
     """
     Create input, output queues, call `function` in a subprocess or a thread.
 
-    `function` is called like this:
-        function(input, output, *args, **kwds)
+    ``function`` is called like this: ``function(input, output, *args, **kwds)``
 
-    Args:
-        use_subprocess: if true, create a new multiprocess;  if false, create
-            a new thread
-        function: the function to call
-        *args, **kwds: arguments to the function.
-        daemon: is the thread or subprocess run as a daemon or not?
+    :param use_subprocess: if true, create a new multiprocess;
+                           if false, create a new thread
+    :param function: the function to call
+    :param daemon: is the thread or subprocess run as a daemon or not?
 
-    Returns:
-        a tuple with three elements: the subprocess or thread, an input
-        queue, and an output queue.
+    :param args: positional arguments to the function
+    :param kwds: keyword arguments to the function
+    :returns: a tuple with three elements: the subprocess or thread, an input
+              queue, and an output queue.
     """
     if use_subprocess:
         Creator, Queue = multiprocessing.Process, multiprocessing.Queue
--- gen_docs.sh
+++ gen_docs.sh
@@ -4,7 +4,19 @@ scripts/extract_bp_help doc/The-bp-Command.rst
 pushd ./doc/
 
 # delete ./api/ dir if exists
-rm -rf ./api || true
-sphinx-apidoc -o api ../bibliopixel/ ../bibliopixel/main/
+rm -rf ./api/ ./_build/ || true
+
+sphinx-apidoc \
+    -o api \
+    ../bibliopixel/ \
+    ../bibliopixel/main/ \
+    ../bibliopixel/util/threads/ \
+    ../bibliopixel/util/colors/arithmetic.py \
+    ../bibliopixel/util/colors/conversions.py \
+    ../bibliopixel/util/colors/classic.py \
+    ../bibliopixel/util/colors/gamma.py \
+    ../bibliopixel/util/colors/juce.py \
+#
+
 make html
 popd
