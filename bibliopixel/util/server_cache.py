6bf5be30950bf7ec142f79aec588b6a0f949b3d3
Tom Ritchford <tom@swirly.com>
2018-03-04 16:00:11 +0100

    Generalize server_cache to allow any key

--- bibliopixel/util/server_cache.py
+++ bibliopixel/util/server_cache.py
@@ -1,35 +1,35 @@
 import errno
-from . import log
+from .. import log
 
 
 ADDRESS_IN_USE_ERROR = """
 
-Port {0} on your machine is already in use.
+Cached server {0} on your machine is already in use.
 Perhaps BiblioPixel is already running on your machine?
 """
 
 CACHED_SERVER_ERROR = """
-Tried to open server of type {new_type} on port {port}, but there was already
+Tried to open server of type {new_type} on {port}, but there was already
 a server of type {old_type} running there.
 """
 
 CACHED_KWDS_WARNING = """
-Cached server for port {server.port} had keywords {server.kwds},
+Cached server for {server.port} had keywords {server.kwds},
 but keywords {kwds} were requested.
 """
 
 
 class _CachedServer:
-    def __init__(self, port, constructor, **kwds):
-        self.server = self._make_server(port, constructor, **kwds)
-        self.port = port
+    def __init__(self, constructor, key, kwds):
+        self.server = self._make_server(constructor, key, kwds)
+        self.key = key
         self.constructor = constructor
         self.kwds = kwds
 
-    def check_keywords(self, constructor, **kwds):
+    def check_keywords(self, constructor, kwds):
         if self.constructor != constructor:
             raise ValueError(CACHED_SERVER_ERROR.format(
-                port=self.port,
+                key=self.key,
                 new_type=str(constructor),
                 old_type=str(self.constructor)))
 
@@ -44,49 +44,55 @@ class _CachedServer:
         return getattr(self.server, key)
 
     @staticmethod
-    def _make_server(port, constructor, **kwds):
+    def _make_server(constructor, key, kwds):
         try:
-            return constructor(port, **kwds)
+            return constructor(key, **kwds)
         except OSError as e:
             if e.errno == errno.EADDRINUSE:
-                e.strerror += ADDRESS_IN_USE_ERROR.format(port)
+                e.strerror += ADDRESS_IN_USE_ERROR.format(key)
                 e.args = (e.errno, e.strerror)
             raise
 
 
 class ServerCache:
     """
-    A class that caches TCP/IP servers by port number so that you don't keep
-    closing and re-opening the same server and interrupting your connection.
-    """
-    ENABLE = True
+    A class that caches servers by key so you don't keep closing and re-opening
+    the same server and interrupting your connection.
 
+    The exact nature of the key depends on the sort of server.
+    For example, for a server socket like SimPixel, it would be just a port
+    number, whereas for a UDP connection like Art-Net, it would be a
+    port, ip_address pair.
+    """
     def __init__(self, constructor, **kwds):
         """
-        :param constructor: a function which takes a port and some keywords
-                            and returns a new TCP/IP server
+        :param constructor: a function which takes a key and some keywords,
+            and returns a new server
         :param kwds: keywords to the ``constructor`` function
         """
         self.servers = {}
-        self.kwds = dict(kwds, constructor=constructor)
+        self.constructor = constructor
+        self.kwds = kwds
 
-    def get_server(self, port):
+    def get_server(self, key):
         """
-        Get a new or existing server for this port.
-        :param port: TCP/IP port for the server to use
-        """
-        if not self.ENABLE:
-            return _CachedServer._make_server(port, **self.kwds)
+        Get a new or existing server for this key.
 
-        server = self.servers.get(port)
+        :param int key: key for the server to use
+        """
+        server = self.servers.get(key)
         if server:
-            server.check_keywords(**self.kwds)
+            # Make sure it's the right server.
+            server.check_keywords(self.constructor, self.kwds)
         else:
-            server = _CachedServer(port, **self.kwds)
-            self.servers[port] = server
+            # Make a new server
+            server = _CachedServer(self.constructor, key, self.kwds)
+            self.servers[key] = server
 
         return server
 
-    def close(self, port):
-        server = self.servers.pop(port)
-        server.server.close()
+    def close(self, key):
+        server = self.servers.pop(key, None)
+        if server:
+            server.server.close()
+            return True
