dbd6a8abf5e6ada09f708b2a59dfe4e63c7a0d02
Tom Ritchford <tom@swirly.com>
2018-04-03 21:15:05 +0200

    Only open SimPixel or Remote page once per session

--- bibliopixel/animation/remote/opener.py
+++ bibliopixel/animation/remote/opener.py
@@ -1,11 +1,18 @@
 import threading, time, webbrowser
 from ... util import log
 
+WEBPAGE_OPENED = False
+
 
 def opener(ip_address, port, delay=1):
     """
     Wait a little and then open a web browser page for the control panel.
     """
+    global WEBPAGE_OPENED
+    if WEBPAGE_OPENED:
+        return
+    WEBPAGE_OPENED = True
+
     def target():
         time.sleep(delay)
         url = 'http://%s:%d' % (ip_address, port)
--- bibliopixel/main/simpixel.py
+++ bibliopixel/main/simpixel.py
@@ -1,10 +1,14 @@
 import webbrowser
 
 DEFAULT_URL = 'http://simpixel.io'
+SIMPIXEL_OPENED = False
 
 
 def open_simpixel(url=None):
-    url = url or DEFAULT_URL
+    global SIMPIXEL_OPENED
+    if not SIMPIXEL_OPENED:
+        url = url or DEFAULT_URL
 
-    if not url.startswith('no'):
-        webbrowser.open(url, new=0, autoraise=True)
+        if not url.startswith('no'):
+            webbrowser.open(url, new=0, autoraise=True)
+            SIMPIXEL_OPENED = True
