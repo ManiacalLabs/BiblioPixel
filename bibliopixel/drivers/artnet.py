686764562e006ae4b53695641c76498ea9e79618
Tom Ritchford <tom@swirly.com>
2018-05-03 15:12:50 +0200

    Filter duplicate outgoing messages in drivers/artnet.py

--- bibliopixel/drivers/artnet.py
+++ bibliopixel/drivers/artnet.py
@@ -6,24 +6,27 @@ from .. util import artnet_message, log, server_cache, udp
 class ArtNet(ServerDriver):
     SERVER_CLASS = udp.Sender
 
-    def __init__(self, *args, ip_address, **kwds):
+    def __init__(self, *args, ip_address, port=artnet_message.UDP_PORT,
+                 filter_dupes=True, **kwds):
         """
         :param dict channel_map: maps DMX channels to positions in
             the color_list
         """
         self.msg = artnet_message.dmx_message()
+        self.last_message = None
 
-        address = ip_address, artnet_message.UDP_PORT
+        address = ip_address, port
         super().__init__(*args, address=address, **kwds)
+        self.filter_dupes = filter_dupes
 
     def _make_buffer(self):
         return self.msg.data
 
     def _send_packet(self):
-        # Regrettably, we need a copy here, because we can't be sure that the
-        # next _render won't come before this message has been sent.
-        # This is avoidable but not easily...
-        self.server.send(copy.copy(self.msg))
+        msg = bytes(self.msg)
+        if not (self.filter_dupes and msg == self.last_message):
+            self.server.send(msg)
+            self.last_message = msg
 
     def _on_positions(self):
         pass
