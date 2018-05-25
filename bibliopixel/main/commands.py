69945270c1b4f549560d7c1553087c10e748d2cd
Tom Ritchford <tom@swirly.com>
2018-05-25 19:38:23 +0200

    New `bp kill` command

--- bibliopixel/main/commands.py
+++ bibliopixel/main/commands.py
@@ -4,8 +4,8 @@ from .. util import log
 
 COMMANDS = (
     'all_pixel', 'all_pixel_test', 'clear_cache', 'color', 'demo', 'devices',
-    'info', 'list', 'load', 'monitor', 'pid', 'remove', 'reset', 'restart',
-    'run', 'save', 'set', 'show', 'shutdown', 'update')
+    'info', 'kill', 'list', 'load', 'monitor', 'pid', 'remove', 'reset',
+    'restart', 'run', 'save', 'set', 'show', 'shutdown', 'update')
 
 MODULES = {c: import_module('bibliopixel.main.' + c) for c in COMMANDS}
 
---
+++ bibliopixel/main/kill.py
@@ -0,0 +1,22 @@
+"""
+Send a kill signal to a BiblioPixel process running on this
+machine to abruptly kill it.
+"""
+
+DESCRIPTION = """
+Example:
+
+``````
+$ bp kill
+``````
+"""
+
+from . restart import run, CHOICES
+
+
+def set_parser(parser):
+    parser.add_argument(
+        'signal', nargs='?', default='SIGKILL', choices=CHOICES,
+        help='Signal to send.')
+
+    parser.set_defaults(run=run)
