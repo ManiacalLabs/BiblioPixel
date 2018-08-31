8c79a03a817da4271e071e4e2e0d589e9e0769a4
Tom Ritchford <tom@swirly.com>
2018-08-31 20:10:41 +0200

    Extract main/args.py

---
+++ bibliopixel/main/args.py
@@ -0,0 +1,59 @@
+import argparse, sys
+from .. util import log
+
+ARGS = None
+
+
+def parse_args(commands, common_flags):
+    helps = '--help', 'help'
+    argv = ['-h' if a in helps else a for a in sys.argv[1:]]
+
+    if not argv:
+        argv = ['-h']
+
+    # argparse doesn't give command-specific help for `bp --help <command>`
+    # so we use `bp <command> --help` (#429)
+    if len(argv) == 2 and argv[0] == '-h':
+        argv.reverse()
+
+    try:
+        argv.remove('--version')
+    except:
+        pass
+    else:
+        log.printer('BiblioPixel version %s' % common_flags.VERSION)
+        if not argv:
+            return
+
+    # Move all the flags to the end.
+    args = [], []
+    for a in argv:
+        args[a.startswith('-')].append(a)
+
+    argv = args[0] + args[1]
+
+    if not argv[0].isidentifier() and '-h' not in argv:
+        # The first argument can't be a command so try to run it.
+        argv.insert(0, 'run')
+
+    if argv and argv[0].startswith('-') and any(
+            not a.startswith('-') for a in argv):
+        log.printer(
+            'bibliopixel: error: command line flags must appear at the end.')
+
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers()
+
+    for name, module in sorted(commands.MODULES.items()):
+        doc = module.__doc__
+        subparser = subparsers.add_parser(name, help=doc)
+        common_flags.add_common_flags(subparser)
+        module.set_parser(subparser)
+        description = getattr(module, 'DESCRIPTION', '')
+        subparser.description = doc + commands.un_md(description)
+
+    if argv == ['-h']:
+        log.printer(commands.HELP)
+
+    global ARGS
+    ARGS = parser.parse_args(argv)
--- bibliopixel/main/main.py
+++ bibliopixel/main/main.py
@@ -1,68 +1,14 @@
-import argparse, os, sys
-from . import commands, common_flags
+import sys
+from . import args, commands, common_flags
 from .. util import log
 from .. project import aliases, project
 
 __all__ = ['main']
 
 
-def get_args(argv=sys.argv):
-    helps = '--help', 'help'
-    argv = ['-h' if a in helps else a for a in argv[1:]]
-
-    if not argv:
-        argv = ['-h']
-
-    # argparse doesn't give command-specific help for `bp --help <command>`
-    # so we use `bp <command> --help` (#429)
-    if len(argv) == 2 and argv[0] == '-h':
-        argv.reverse()
-
-    try:
-        argv.remove('--version')
-    except:
-        pass
-    else:
-        log.printer('BiblioPixel version %s' % common_flags.VERSION)
-        if not argv:
-            return
-
-    # Move all the flags to the end.
-    args = [], []
-    for a in argv:
-        args[a.startswith('-')].append(a)
-
-    argv = args[0] + args[1]
-
-    if not argv[0].isidentifier() and '-h' not in argv:
-        # The first argument can't be a command so try to run it.
-        argv.insert(0, 'run')
-
-    if argv and argv[0].startswith('-') and any(
-            not a.startswith('-') for a in argv):
-        log.printer(
-            'bibliopixel: error: command line flags must appear at the end.')
-
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers()
-
-    for name, module in sorted(commands.MODULES.items()):
-        doc = module.__doc__
-        subparser = subparsers.add_parser(name, help=doc)
-        common_flags.add_common_flags(subparser)
-        module.set_parser(subparser)
-        description = getattr(module, 'DESCRIPTION', '')
-        subparser.description = doc + commands.un_md(description)
-
-    if argv == ['-h']:
-        log.printer(commands.HELP)
-
-    return parser.parse_args(argv)
-
-
 def main():
-    args = get_args()
-    run = getattr(args, 'run', None)
+    args.parse_args(commands, common_flags)
+    run = getattr(args.ARGS, 'run', None)
     if not run:
         log.printer('ERROR: No command entered')
         log.printer('Valid commands are:')
@@ -73,10 +19,10 @@ def main():
         log.printer('    bp --help')
         sys.exit(-1)
 
-    common_flags.execute_args(args)
+    common_flags.execute_args(args.ARGS)
 
     try:
-        result = run(args) or 0
+        result = run(args.ARGS) or 0
     except Exception as e:
         if args.verbose:
             raise
