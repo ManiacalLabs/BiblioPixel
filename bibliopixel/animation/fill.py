4f306d42c1126f1adaf23896465aa81601479aee
Tom Ritchford <tom@swirly.com>
2018-10-13 21:12:13 +0200

    Use the palette in animation/fill

--- bibliopixel/animation/fill.py
+++ bibliopixel/animation/fill.py
@@ -1,32 +1,20 @@
-from . animation import BaseAnimation
-from .. util import colors
+from . animation import Animation
+from .. util.colors import make
 
 
-class Fill(BaseAnimation):
+class Fill(Animation):
     """
-    Fill the screen with a single color.
+    Fill the screen with a single color or a palette of colors
     """
-    def __init__(self, *args, color='black', **kwds):
-        super().__init__(*args, preclear=False, **kwds)
+    def __init__(self, *args, color=None, palette=None, **kwds):
+        # Handle legacy "color" parameter
+        if color:
+            if palette:
+                raise ValueError('At most one of color and pallete can be set')
+            palette = make.colors(color)
 
-        is_numpy = hasattr(self.color_list, 'dtype')
-        self._set_color = self._set_numpy if is_numpy else self._set_classic
-        self._color = color
+        super().__init__(*args, preclear=False, palette=palette, **kwds)
 
-    def pre_run(self):
-        self.color = self._color
-
-    @property
-    def color(self):
-        return self._color
-
-    @color.setter
-    def color(self, color):
-        self._color = colors.to_color(color)
-        self._set_color()
-
-    def _set_numpy(self):
-        self.color_list[:None] = self._color
-
-    def _set_classic(self):
-        self.color_list[:] = [self._color] * len(self.color_list)
+    def step(self, amt=1):
+        n = len(self.color_list)
+        self.color_list[:] = (self.palette.get(i) for i in range(n))
