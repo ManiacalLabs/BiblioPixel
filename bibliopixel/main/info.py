314293821ba8ae0eee24d6f2b4eaf6d2f0341e48
Tom Ritchford <tom@swirly.com>
2018-10-27 12:15:22 +0200

    Improvements to `bp info`
    * Print /proc/cpuinfo if it exists
    * More information about platforms

--- bibliopixel/main/info.py
+++ bibliopixel/main/info.py
@@ -10,46 +10,72 @@ that the program is running on.
 import datetime, os, platform, sys
 import bibliopixel, BiblioPixelAnimations, loady
 from bibliopixel.util import log
+from bibliopixel.util.platform import Platform
 
 NONE = '(none)'
+MODULES = bibliopixel, BiblioPixelAnimations, loady
 
 
 def run(args):
+    platform = Platform()
+
     now = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S')
-    log.printer('Timestamp:     ', now)
-    log.printer('Python version:', platform.python_version())
-    log.printer('Platform:      ', platform.platform())
-    log.printer('`bp` path:     ', sys.argv[0])
-    log.printer('Library path:  ', os.path.dirname(bibliopixel.__file__))
+    bp_path = sys.argv[0]
+    library_path = os.path.dirname(bibliopixel.__file__)
+    dependencies = '\n'.join(_dependency(m) for m in MODULES)
+    cpuinfo = platform.cpuinfo or '' and CPUINFO % '\n'.join(platform.cpuinfo)
 
-    log.printer()
-    log.printer('Dependencies:')
-    for module in bibliopixel, BiblioPixelAnimations, loady:
-        path = os.path.dirname(module.__file__)
-        parent = os.path.dirname(path)
-
-        try:
-            fp = open(os.path.join(path, 'VERSION'))
-        except:
-            try:
-                fp = open(os.path.join(parent, 'VERSION'))
-            except:
-                fp = None
-
-        version = fp.read().strip() if fp else NONE
-
-        try:
-            import git
-            repo = git.Repo(os.path.dirname(path))
-        except:
-            commit_id = tag = NONE
-        else:
-            commit_id = repo.commit('HEAD').hexsha[:7]
-            tag = repo.tags[-1].name if repo.tags else '(none)'
-
-        log.printer('    %s: version %s, git commit: %s, git tag %s' %
-                    (module.__name__, version, commit_id, tag))
+    log.printer(MESSAGE.format(**locals()))
 
 
 def set_parser(parser):
     parser.set_defaults(run=run)
+
+
+def _dependency(module):
+    path = os.path.dirname(module.__file__)
+    parent = os.path.dirname(path)
+
+    try:
+        fp = open(os.path.join(path, 'VERSION'))
+    except:
+        try:
+            fp = open(os.path.join(parent, 'VERSION'))
+        except:
+            fp = None
+
+    version = fp.read().strip() if fp else NONE
+
+    try:
+        import git
+        repo = git.Repo(os.path.dirname(path))
+    except:
+        commit_id = tag = NONE
+    else:
+        commit_id = repo.commit('HEAD').hexsha[:7]
+        tag = repo.tags[-1].name if repo.tags else '(none)'
+
+    return ('    %s: version %s, git commit: %s, git tag %s' %
+            (module.__name__, version, commit_id, tag))
+
+
+MESSAGE = """\
+Timestamp:        {now}
+Python version:   {platform.python_version}
+`bp` path:        {bp_path}
+Library path:     {library_path}
+
+Platform:         {platform.platform}
+Platform version: {platform.platform_version}
+Platform release: {platform.release}
+
+Dependencies:
+{dependencies}
+{cpuinfo}"""
+
+CPUINFO = """
+cpuinfo
+--------------------------------------------------------------------------------
+%s
+--------------------------------------------------------------------------------
+"""
---
+++ bibliopixel/util/platform.py
@@ -0,0 +1,35 @@
+import platform
+
+MAC = 'Darwin'
+WINDOWS = 'Windows'
+CPUINFO_FILE = '/proc/cpuinfo'
+
+
+class Platform:
+    def __init__(self):
+        self.platform = platform.system()
+        self.version = platform.version()
+        self.release = platform.release()
+        self.python_version = platform.python_version()
+
+        try:
+            self.cpuinfo = [i.strip() for i in open(CPUINFO_FILE)]
+        except:
+            self.cpuinfo = []
+
+        def is_rpi_line(i):
+            return i.startswith('Hardware') and i.endswith('BCM2708')
+
+        self.is_raspberry_pi = any(is_rpi_line(i) for i in self.cpuinfo)
+        self.is_linux = (self.platform == 'linux')
+
+        platform_version = ()
+        if self.is_linux:
+            # Use the linux distribution as the name
+            self.platform = platform.linux_distribution()[0].lower()
+        elif self.platform == WINDOWS:
+            platform_version = platform.win32_ver()
+        elif self.platform == MAC:
+            release, versioninfo, machine = platform.mac_ver()
+            platform_version = release, machine
+        self.platform_version = ':'.join(platform_version)
