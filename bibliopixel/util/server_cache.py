4c7b93b6e829d36da7e86ddfb0a0c261f273cba0
Tom Ritchford <tom@swirly.com>
2018-10-31 10:34:01 +0100

    Add kwds to server_cache.get_server

--- bibliopixel/util/server_cache.py
+++ bibliopixel/util/server_cache.py
@@ -35,19 +35,20 @@ class ServerCache:
         self.constructor = constructor
         self.kwds = kwds
 
-    def get_server(self, key):
+    def get_server(self, key, **kwds):
         """
         Get a new or existing server for this key.
 
         :param int key: key for the server to use
         """
+        kwds = dict(self.kwds, **kwds)
         server = self.servers.get(key)
         if server:
             # Make sure it's the right server.
-            server.check_keywords(self.constructor, self.kwds)
+            server.check_keywords(self.constructor, kwds)
         else:
             # Make a new server
-            server = _CachedServer(self.constructor, key, self.kwds)
+            server = _CachedServer(self.constructor, key, kwds)
             self.servers[key] = server
 
         return server
