8ac85f5dc14b2d50a7f3ec1b7239efaa923ab008
Tom Ritchford <tom@swirly.com>
2018-12-02 13:26:03 +0100

    Now classic release_test no longer has to be run separately

--- scripts/release_test/tests/__init__.py
+++ scripts/release_test/tests/__init__.py
@@ -1,6 +1,7 @@
 from . import (
-    builder, demo, new, unit, simpixel, rest, remote, keyboard, j12k, midi,
-    all_pixel, bp, list)
+    classic, builder, demo, new, unit, simpixel, rest, remote,
+    keyboard, j12k, midi, all_pixel, bp, list)
 
-__all__ = ['builder', 'demo', 'new', 'unit', 'simpixel', 'keyboard', 'rest',
-           'midi', 'j12k', 'bp', 'all_pixel', 'remote', 'list']
+__all__ = [
+    'classic', 'builder', 'demo', 'new', 'unit', 'simpixel', 'rest', 'remote',
+    'keyboard', 'j12k', 'midi', 'all_pixel', 'bp', 'list']
--- scripts/release_test/tests/classic.py
+++ scripts/release_test/tests/classic.py
@@ -1,30 +1,39 @@
 #!/usr/bin/env python3
 
-# This must be run separately from the command line like this:
-#
-#   $ scripts/release_test/tests/classic.py
 
-import bibliopixel
-from bibliopixel.drivers.simpixel import SimPixel
+def main():
+    import bibliopixel
+    from bibliopixel.drivers import simpixel
 
-bibliopixel.log.setLogLevel(bibliopixel.log.DEBUG)
+    bibliopixel.log.setLogLevel(bibliopixel.log.DEBUG)
 
-# set number of pixels & LED type here
-driver = SimPixel(num=32)
+    # set number of pixels & LED type here
+    driver = simpixel.SimPixel(num=32)
 
-# load the LEDStrip class
-from bibliopixel.layout import Strip
-led = Strip(driver)
+    # load the LEDStrip class
+    from bibliopixel.layout import Strip
+    led = Strip(driver)
 
-# load channel test animation
-from bibliopixel.animation import StripChannelTest
-anim = StripChannelTest(led)
+    # load channel test animation
+    from bibliopixel.animation import StripChannelTest
+    anim = StripChannelTest(led)
+    driver.open_browser()
 
-try:
-    # run the animation
-    anim.run()
+    try:
+        # run the animation
+        anim.run(seconds=4)
 
-except KeyboardInterrupt:
-    # Ctrl+C will exit the animation and turn the LEDs offs
-    led.all_off()
-    led.update()
+    finally:
+        # Ctrl+C will exit the animation and turn the LEDs offs
+        led.all_off()
+        led.update()
+
+
+def run():
+    import common
+    common.test_prompt('classic')
+    common.execute('python', __file__)
+
+
+if __name__ == '__main__':
+    main()
